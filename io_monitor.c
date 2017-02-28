96aa7019c08cc38c69d38f473c32618f566c6e21
Maciej Kaminski <maciej.kaminski@intel.com>
2017-02-28 17:58:12 -0600

    Added headers to output from listener
    Signed-off-by: Maciej Kaminski <maciej.kaminski@intel.com>

--- io_monitor.c
+++ io_monitor.c
@@ -51,6 +51,8 @@
 #include <sys/mount.h>
 #include <sys/stat.h>
 #include <sys/time.h>
+#include "ops.h"
+#include "domains.h"
 
 
 // to build:
@@ -89,6 +91,8 @@ gettimeofday(&end_time, NULL);
 #define TIME_AFTER() \
 &end_time
 
+
+
 typedef struct _MONITOR_MESSAGE
 {
    long message_type;
@@ -110,75 +114,6 @@ static const int message_project_id = 'm';
 static key_t message_queue_key = -1;
 static int message_queue_id = -1;
 
-
-// set up some categories to group metrics
-typedef enum {
-   LINKS,             // 0  (link, unlink)
-   XATTRS,            // 1  (getxattr, setxattr, listxattr)
-   DIRS,              // 2  (mkdir, rmdir, chdir)
-   FILE_SYSTEMS,      // 3  (mount, umount)
-   FILE_DESCRIPTORS,  // 4  (dup, fcntl)
-   SYNCS,             // 5  (sync, flush)
-   SOCKETS,           // 6  (socket, accept, bind, connect, send, recv)
-   SEEKS,             // 7  (fseek, lseek)
-   FILE_SPACE,        // 8  (fallocate, ftruncate)
-   PROCESSES,         // 9  (fork, exec, kill, exit)
-   FILE_METADATA,     // 10  (stat, access, chmod, chown)
-   FILE_WRITE,        // 11  (write)
-   FILE_READ,         // 12  (read)
-   FILE_OPEN_CLOSE,   // 13  (open, close)
-   MISC,              // 14  (miscellaneous; rename, flock, mknod, chroot, etc.)
-   DIR_METADATA       // 15  (opendir, readdir, seekdir, etc.)
-} DOMAIN_TYPE;
-
-// define the operations that we're tracking. please note that in some
-// cases several C library calls all get identified the same (e.g., open,
-// open64, fopen are all recorded as OPEN).
-typedef enum {
-   OPEN,           // 0  (FILE_OPEN_CLOSE)
-   CLOSE,          // 1  (FILE_OPEN_CLOSE)
-   WRITE,          // 2  (FILE_WRITE)
-   READ,           // 3  (FILE_READ)
-   SYNC,           // 4  (SYNCS)
-   SEEK,           // 5  (SEEKS)
-   RENAME,         // 6  (MISC)
-   LINK,           // 7  (LINKS)
-   UNLINK,         // 8  (LINKS)
-   FCNTL,          // 9  (FILE_DESCRIPTORS)
-   DUP,            // 10  (FILE_DESCRIPTORS)
-   STAT,           // 11  (FILE_METADATA)
-   ACCESS,         // 12  (FILE_METADATA)
-   CHMOD,          // 13  (FILE_METADATA)
-   CHOWN,          // 14  (FILE_METADATA)
-   FLOCK,          // 15  (MISC)
-   READLINK,       // 16  (LINKS)
-   UTIME,          // 17  (FILE_METADATA)
-   GETXATTR,       // 18  (XATTRS)
-   SETXATTR,       // 19  (XATTRS)
-   LISTXATTR,      // 20  (XATTRS)
-   REMOVEXATTR,    // 21  (XATTRS)
-   MOUNT,          // 22  (FILE_SYSTEMS)
-   UMOUNT,         // 23  (FILE_SYSTEMS)
-   FORK,           // 24  (PROCESSES)
-   MKNOD,          // 25  (MISC)
-   MKDIR,          // 26  (DIRS)
-   RMDIR,          // 27  (DIRS)
-   CHDIR,          // 28  (DIRS)
-   CHROOT,         // 29  (MISC)
-   SOCKET,         // 30  (SOCKETS)
-   _IO_NEW_FOPEN,  // 31  (FILE_OPEN_CLOSE)
-   FLUSH,          // 32  (SYNCS)
-   ALLOCATE,       // 33  (FILE_SPACE)
-   TRUNCATE,       // 34  (FILE_SPACE)
-   OPENDIR,        // 35  (DIR_METADATA)
-   CLOSEDIR,       // 36  (DIR_METADATA)
-   READDIR,        // 37  (DIR_METADATA)
-   SEEKDIR,        // 38  (DIR_METADATA)
-   TELLDIR,        // 39  (DIR_METADATA)
-   DIRFD,          // 40  (DIR_METADATA)
-   REWINDDIR,      // 41  (DIR_METADATA)
-   SCANDIR         // 42  (DIR_METADATA)
-} OP_TYPE;
 
 
 // a debugging aid that we can easily turn off/on
